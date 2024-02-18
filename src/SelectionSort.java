45d00f3b6297e171e3a491f3d71808d2b61a5abd
Abhinav Selvarajan <91642109+esenopetra@users.noreply.github.com>
2024-02-18 12:56:09 +0530

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-public class SelectionSort{
-    public static void main(String[] args){
-        int[] a = {7,4,10,8,3,1};
-        int n = a.length;
-        for(int i=0; i<n-1; i++){
-            min = i;
-            for(int j = i+1; j<n; j++){
-                if(a[min]>a[j]){
-                    min = j;
-                }
-            }
-            if(min != i){
-                swap(a[min],a[i]);
-            }
-        }
-    }
-    int swap(int a, int b){
-        int t = a;
-            a = b;
-            b = a;
-    }
-}
-=======
+
 public class SelectionSort {
     
     public static void main(String[] args){
@@ -45,4 +22,3 @@ public class SelectionSort {
     }
     
 }
->>>>>>> 4bd75ad3282fd279618cee7dc8188e6417d5e391
